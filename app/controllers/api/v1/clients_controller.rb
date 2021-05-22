29fc092c5c7ffa10f38f998d742b0ae08bf0cbe8
Ignacio Moran <moran.ua@gmail.com>
2021-05-21 23:10:18 -0400

    uf and client endpoint created

--- app/controllers/api/v1/clients_controller.rb
+++ app/controllers/api/v1/clients_controller.rb
@@ -1,5 +1,10 @@
-class ClientsController < ApplicationController
+class Api::V1::ClientsController < ApplicationController
   before_action :set_client, only: [:show, :update, :destroy]
+
+  def by_name
+    @client = Client.find_by(name: params[:name])
+    render json: @client
+  end
 
   # GET /clients
   def index
--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,11 @@ Rails.application.routes.draw do
     end
   end
 
+  namespace :api do
+    namespace :v1 do
+      get 'by_name/:name', to: 'clients#by_name'
+    end
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
